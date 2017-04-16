9c8a174a8d9fe0997b664fc621d35e0936ed6dde
Minnnat <minnatjmitul@gmail.com>
2017-04-16 11:58:01 +0600

    DELETED cOMMENT BY HTTTRACK

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xml:lang="en" lang="en" xmlns="http://www.w3.org/1999/xhtml">
   
-<!-- Mirrored from www.ia.omron.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 15 Dec 2016 14:23:32 GMT -->
-<!-- Added by HTTrack --><!-- /Added by HTTrack -->
+
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta http-equiv="content-type" content="text/html;charset=utf-8" />
@@ -639,5 +638,4 @@
     </div>
   </body>
 
-<!-- Mirrored from www.ia.omron.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 15 Dec 2016 14:23:40 GMT -->
 </html>
