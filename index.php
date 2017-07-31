403b3644e6e70e7ab66f9c5bf971b36d3a0f6003
Jannat012 <juicse@yahoo.com>
2017-07-31 11:25:11 +0600

    modify

--- index.php
+++ index.php
@@ -114,7 +114,7 @@
           <ul>
             <li><a href="#" class="mainImageTitle"><img src="resources/images/icon_01.gif" width="3" height="9" alt="" class="iconA01"/>Dhaka</a>
               </li>
-            <li><a href="../omron/HTML/index.html" class="mainImageTitle"><img src="resources/images/icon_01.gif" width="3" height="9" alt="" class="iconA01"/>Chittagong</a>
+            <li><a href="../omron/ctg/index.html" class="mainImageTitle"><img src="resources/images/icon_01.gif" width="3" height="9" alt="" class="iconA01"/>Chittagong</a>
             </li>
           </ul>
         </div>
